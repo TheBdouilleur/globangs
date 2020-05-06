877d9f57b371e2ba2d5e05d19237a0b7d4a92d56
TheBdouilleur <bdouilleur@gmail.com>
2020-05-06 21:10:45 +0200

    Rearranged everything to use `pynput` instead of `keyboard`, changed keyboard shortcut to Alt Space F to avoid conflict with other  apps

--- bangs.py
+++ bangs.py
@@ -1,7 +1,8 @@
 #usr/bin/python3.8
-import keyboard
+
+'''1. Core program'''
 import os
-import json
+from pynput import keyboard
 
 bangs = {'!g':'google', '!w':'wikipedia'}
 
@@ -11,11 +12,37 @@ def formatWithBangs(text):
 
     return text
 
-keyboard.add_hotkey('ctrl+space+f', formatWithBangs, args=('!g'))
-#while True:
-#    selectedText = os.popen('xsel').read()
-#
-#    if '!' in selectedText and not '! ' in selectedText:
-#        newText = formatWithBangs(selectedText)
-#        pyautogui.typewrite(newText)
-keyboard.wait()
+def getSelection():
+    selectedText = os.popen('xsel').read()  
+
+    if selectedText and '!' in selectedText and not '! ' in selectedText:
+        return selectedText
+
+def autoComplete(): #Wrapper function necessary to be properly called back by add_hotkey()
+    formattedText = formatWithBangs(getSelection()) 
+    kbd = keyboard.Controller()
+    kbd.type(formattedText) 
+
+
+'''2. Keyboard shortcut handling'''
+COMBINATION = {keyboard.Key.alt, keyboard.Key.space, keyboard.KeyCode.from_char('f')}
+current = set()
+
+def on_press(key):
+    if key in COMBINATION:
+        current.add(key)
+        if all(k in current for k in COMBINATION):
+            print('All modifiers active!')
+            autoComplete()
+    if key == keyboard.Key.esc:
+        listener.stop()
+
+def on_release(key):
+    try:
+        current.remove(key)
+    except KeyError:
+        pass
+
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
