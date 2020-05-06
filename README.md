9c6b0bfb05b6ccc0f41bbb4e18213b80073aa5c4
TheBdouilleur <bdouilleur@gmail.com>
2020-05-06 18:14:39 +0200

    README completed, switched the bg task to a more efficient `keyboard.wait()`

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-globangs
+# What is globangs ?
+globangs is a program inspired by [DuckDuckGo !Bangs](https://duckduckgo.com/bangs)
--- bangs.py
+++ bangs.py
@@ -1,22 +1,21 @@
 #usr/bin/python3.8
+import keyboard
 import os
+import json
+
 bangs = {'!g':'google', '!w':'wikipedia'}
 
 def formatWithBangs(text):
-#    wordList = list(text)[0].split()
-#    text.replace(bangs[text])
-#    curatedList = wordList[wordList.index('!')
-    print('prepass:', text)
     for bang in bangs:
-        print('passing', bang)
         text = text.replace(bang, bangs[bang])
 
     return text
 
-while True:
-    selectedText = os.popen('xsel').read()
-    if '!' in selectedText and not '! ' in selectedText:
-        print(selectedText)#formatWithBangs(selectedText)
-        newText = formatWithBangs(selectedText)
-        print(newText)
-        break
+keyboard.add_hotkey('ctrl+space+f', formatWithBangs, args=('!g'))
+#while True:
+#    selectedText = os.popen('xsel').read()
+#
+#    if '!' in selectedText and not '! ' in selectedText:
+#        newText = formatWithBangs(selectedText)
+#        pyautogui.typewrite(newText)
+keyboard.wait()
