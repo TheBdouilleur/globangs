65ddc81e229b78eb49bcc0d4b16a29b84a0b3036
TheBdouilleur <bdouilleur@gmail.com>
2020-05-08 15:15:49 +0200

    Cleaned code; added json support for bangs, prints

--- bangs.py
+++ bangs.py
@@ -3,26 +3,29 @@
 '''1. Core program'''
 import os
 from pynput import keyboard
+import json
 
-bangs = {'!g':'google', '!w':'wikipedia'}
+
+with open('settings.json', "r") as file:
+    config = json.load(file)
 
 def formatWithBangs(text):
+    bangs = config['bangs']
     for bang in bangs:
         text = text.replace(bang, bangs[bang])
 
     return text
 
-def getSelection():
-    selectedText = os.popen('xsel').read()  
 
-    if selectedText and '!' in selectedText and not '! ' in selectedText:
-        return selectedText
+def autoComplete(): #Wrapper function necessary to be properly called back
+    #TODO: move the if else to root (ie. os.popen(...)) to increase speed and reduce memory impact
+    formattedText = formatWithBangs(os.popen('xsel').read()) 
 
-def autoComplete(): #Wrapper function necessary to be properly called back by add_hotkey()
-    formattedText = formatWithBangs(getSelection()) 
-    kbd = keyboard.Controller()
-    kbd.type(formattedText) 
+    if formattedText and '!' in formattedText and not '! ' in formattedText:
+        kbd = keyboard.Controller()
+        kbd.type(formattedText) 
 
+    else: print("autoComplete() didn't detect bangs in current selection")
 
 '''2. Keyboard shortcut handling'''
 COMBINATION = {keyboard.Key.alt, keyboard.Key.space, keyboard.KeyCode.from_char('f')}
@@ -32,10 +35,9 @@ def on_press(key):
     if key in COMBINATION:
         current.add(key)
         if all(k in current for k in COMBINATION):
-            print('All modifiers active!')
+            print('Keyboard shortcut pressed, running autoComplete()')
             autoComplete()
-    if key == keyboard.Key.esc:
-        listener.stop()
+    
 
 def on_release(key):
     try:
